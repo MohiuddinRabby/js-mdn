dbc0d0fc25c725c2055d1b3b160a319cfff11bd8
MohiuddinRabby <rabbymohiuddin@gmail.com>
2020-04-26 02:18:34 +0600

    array

---
+++ 02_array/app.js
@@ -0,0 +1,15 @@
+let shopping = ['bread','milk','cheese','butter']
+console.log(shopping)
+let sequence = [1,2,3,4,5,6,7]
+//multidimensional array
+let random = ['tree',1,66,[0,1,2,3,4]]
+console.log(random[3][2])
+//2
+//Converting between strings and arrays
+let myData = 'Manchester,London,Liverpool,Birmingham,Leeds,Carlisle';
+let myArr = myData.split(',')
+console.log(myArr)
+//push pop
+let myArray = ['Manchester', 'London', 'Liverpool', 'Birmingham', 'Leeds', 'Carlisle'];
+myArray.push('cardiff')
+console.log(myArray)
