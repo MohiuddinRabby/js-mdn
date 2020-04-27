aa64cdd9bf94934c44ba9aae3ed6e3fb3be0722c
MohiuddinRabby <rabbymohiuddin@gmail.com>
2020-04-27 23:39:33 +0600

    oojs

---
+++ 05_function/app.js
@@ -0,0 +1,17 @@
+//////////////////////////////
+//built in browser function
+let myText = 'i am a string';
+let newString = myText.replace('string','sausage');
+console.log(newString);
+/////////////////////////////
+
+////////////////////////////
+let myArray = ['i','likes','to','eat','pizza'];
+let madeAString = myArray.join(' ')
+console.log(madeAString);
+///////////////////////////
+
+///////////////////////////
+let myNumber = Math.floor(Math.random()*100)
+// console.log(myNumber)
+//////////////////////////
---
+++ 07_oojs/app.js
@@ -0,0 +1,12 @@
+//////////////////////////////
+class Shape {
+    constructor(name, sides, sideLength) {
+        this.name = name;
+        this.sides = sides;
+        this.sideLength = sideLength;
+
+    }
+    
+}
+let s = new Shape('square',4,5)
+console.log(s)
