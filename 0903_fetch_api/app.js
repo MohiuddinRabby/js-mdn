918b390b6b963ddca8db53ffc869b58793b70f71
MohiuddinRabby <rabbymohiuddin@gmail.com>
2020-04-30 16:26:47 +0600

    fetch api added

---
+++ 0903_fetch_api/app.js
@@ -0,0 +1,34 @@
+document.getElementById('text-data').addEventListener('click',getTextData);
+document.getElementById('json-data').addEventListener('click',getJsonData);
+//get text data
+function getTextData(e){
+    fetch('data.txt')
+    .then(function(res){
+        return res.text();
+    })
+    .then(function(data){
+        console.log(data)
+        document.querySelector('.output').innerHTML=data;
+    })
+    e.preventDefault();
+
+}
+//get json data
+
+function getJsonData(){
+    fetch('post.json')
+    .then(function(res){
+        return res.json();
+    })
+    .then(function(data){
+        console.log(data);
+        let output= '';
+        data.forEach(function(post){
+            output += `
+            <li><b>${post.title}</b></li>
+            <li>${post.body}</li>
+            `
+        })
+        document.querySelector('.output').innerHTML=output
+    })
+}
